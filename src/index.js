59abfe91205fcb29df31562d34ac46df5cc7d6e8
Kenta Iwasaki <iwaken59371518@gmail.com>
2022-04-25 21:58:43 +0900

    add gui

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as BABYLON from 'babylonjs';
+import * as GUI from 'babylonjs-gui';
 import 'babylonjs-materials';
 import 'babylonjs-loaders';
 import 'babylonjs-inspector';
@@ -10,6 +11,7 @@ const createScene = () => {
     const scene = new BABYLON.Scene(engine);
     // var hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("./assets/environment.dds", scene);
     // var currentSkybox = scene.createDefaultSkybox(hdrTexture, true);
+   // const ground = BABYLON.MeshBuilder.CreateGround("ground", options, scene); 
     const camera = new BABYLON.ArcRotateCamera("camera", 3*Math.PI/4, Math.PI/3, 2.1, new BABYLON.Vector3(-0.35, 0.7, 0.8));
     camera.attachControl(canvas, true);
     BABYLON.SceneLoader.Append("assets/", "chair.glb", scene, function (scene) {
@@ -22,8 +24,8 @@ const createScene = () => {
         camera.speed = 0.4204;
         camera.lowerRadiusLimit = 0.0210;
     });
+    addUI(scene);
 
-    
     return scene;
 }
 const scene = createScene(); //Call the createScene function
@@ -39,3 +41,38 @@ engine.runRenderLoop(function () {
 window.addEventListener("resize", function () {
     engine.resize();
 });
+
+function addUI(scene){
+    var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    
+    var panel = new GUI.StackPanel();
+    panel.width = "200px";
+    panel.isVertical = true;
+    panel.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+    panel.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+    advancedTexture.addControl(panel);
+
+    
+    var picker = new GUI.ColorPicker();
+    picker.value = scene.clearColor;
+    picker.height = "150px";
+    picker.width = "150px";
+    picker.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+    picker.onValueChangedObservable.add(function(value) { // value is a color3
+        scene.clearColor = value;
+    });
+
+    var checkbox = new GUI.Checkbox();
+    checkbox.width = "20px";
+    checkbox.height = "20px";
+    checkbox.isChecked = true;
+    checkbox.color = "green";
+    checkbox.onIsCheckedChangedObservable.add(function(value) {
+        // if (skull) {
+        //     skull.useVertexColors = value;
+        // }
+    });
+    panel.addControl(checkbox);
+
+    panel.addControl(picker);     
+} 
