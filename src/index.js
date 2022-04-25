f3c05ce200e923aedc038055b404b6efed24a7b7
Kenta Iwasaki <iwaken59371518@gmail.com>
2022-04-25 19:13:41 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import * as BABYLON from 'babylonjs';
+import 'babylonjs-materials';
+const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+// Add your code here matching the playground format
+const createScene = () => {
+    const scene = new BABYLON.Scene(engine);
+    // var hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("./assets/environment.dds", scene);
+    // var currentSkybox = scene.createDefaultSkybox(hdrTexture, true);
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+    BABYLON.SceneLoader.Append("assets/", "chair.glb", scene, function (scene) {
+        scene.createDefaultCameraOrLight(true, true, true);
+        scene.activeCamera.alpha += Math.PI;
+    });
+    return scene;
+}
+const scene = createScene(); //Call the createScene function
+
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+    scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+    engine.resize();
+});
